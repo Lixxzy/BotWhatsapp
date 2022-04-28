3e5ffb699535b9cf17c817649c972935d3dbdd53
Lixxzy <104194374+Lixxzy@users.noreply.github.com>
2022-04-29 06:48:39 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,11 +3,11 @@ const PhoneNumber = require('awesome-phonenumber')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
-    "displayName": "Iyann Ganteng",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Iyann Ganteng\nitem1.TEL;waid=6285742344873:6285742344873\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:kastaraiyan@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/IyanZ-XcX\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia\nitem5.X-ABLabel:───────[ Tohka Yatogami ]───────\nEND:VCARD"
+    "displayName": "[Owner]• Iyann No Limit",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Iyann No Limit\nitem1.TEL;waid=6285742344873:6285742344873\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:iyannkastara@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Lixxzy\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia\nitem5.X-ABLabel:───────[ NIGTH ]───────\nEND:VCARD"
   }, {
-    "displayName": "Whatsapp",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
+    "displayName": "[Partners]• Neha Cantikk",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Neha Cantikk\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Partners\nitem2.EMAIL;type=INTERNET:neha@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://github.com/Lixxzy/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Wibu;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Partners\nitem5.X-ABLabel:───────[ NIGTH ]───────\nEND:VCARD"
     }]
   }, MessageType.contactsArray, { quoted: m })
 }
